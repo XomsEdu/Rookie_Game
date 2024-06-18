569147657ef94ac08f75dda6b0d8ffc3f5eecc9f
XomsEdu <vakhrameiev.b1206@lntu.edu.ua>
2024-06-19 00:23:46 +0300

    Bullet update
    +логіка для кулі (тепер снаряди деспавняться через 5 секунд або при зіткнені з стіною)

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    public float lifetime = 10f; // Час життя кулі
+    public LayerMask collisionLayers; // Маска слоїв для перевірки зіткнень
+
+    private void Start()
+    {
+        // Знищити кулю коли час закінчиться
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Перевірка чи шар об'єкта, з яким відбулося зіткнення, входить до маски слоїв
+        if ((collisionLayers.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            // Знищити кулю при зіткненні з об'єктом
+            Destroy(gameObject);
+        }
+    }
+}
